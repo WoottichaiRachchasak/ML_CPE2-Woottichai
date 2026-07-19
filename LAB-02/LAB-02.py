b84b5c948a258dbdd0fdaf84292356f03285b6b5
Woottichai Rachchasak <kingfblf@gmail.com>
2026-07-19 23:38:48 +0700

    Lab_02

--- LAB-02/LAB-02.py
+++ LAB-02/LAB-02.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 #LAB_01 Dataset Exploration
 # - Load Dataset
 df = pd.read_csv("openpowerlifting.csv")
@@ -55,12 +56,72 @@ for col in numeric_cols:
         col_median = df_cleaned[col].median()
         # เปลี่ยนมาใช้ df_cleaned แก้ไขค่าตรงๆ แบบนี้
         df_cleaned[col] = df_cleaned[col].fillna(col_median)
-        print(f"-> คอลัมน์ '{col}' พบค่าว่าง {missing_count} จุด: เติมเต็มด้วยค่า Median ({col_median:.2f}) เรียบร้อย")
+        print(f"-> colum '{col}' missing count: {missing_count} Filled with median value ({col_median:.2f})")
 categorical_cols = df_cleaned.select_dtypes(include=['object']).columns
 for col in categorical_cols:
     missing_count = df_cleaned[col].isnull().sum()
     if missing_count > 0:
         col_mode = df_cleaned[col].mode()[0]
         df_cleaned[col] = df_cleaned[col].fillna(col_mode)
-        print(f"-> คอลัมน์ '{col}' พบค่าว่าง {missing_count} จุด: เติมเต็มด้วยค่า Mode ('{col_mode}') เรียบร้อย")
+        print(f"colum '{col}' missing count: {missing_count} : Filled with mode value ('{col_mode}')")
+        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
 # - Duplicate Removal
+rows_before = df_cleaned.shape[0]
+df_cleaned = df_cleaned.drop_duplicates()
+rows_after = df_cleaned.shape[0]
+print(f"before delete: {rows_before} row | after delete: {rows_after} row")
+print(f"Deleted data: {rows_before - rows_after} rows")
+print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+# - Incorrect Data Correction
+for col in numeric_cols:
+    incorrect_mask = df_cleaned[col] < 0
+    incorrect_count = incorrect_mask.sum()
+    if incorrect_count > 0:
+        col_median = df_cleaned[col].median()
+        df_cleaned.loc[incorrect_mask, col] = col_median
+        print(f"-> Detected {incorrect_count} erroneous negative values ​​in column '{col}': corrected to the median value")
+    else:
+        print(f"-> Column '{col}': No anomalous negative values ​​found")
+# - Data Type Conversion
+if 'Age' in df_cleaned.columns:
+    df_cleaned['Age'] = df_cleaned['Age'].round().astype('int64')
+    print("-> Converted column 'Age' from Float to Integer")
+    print("-> Converted column 'Age' from Float to Integer ")
+
+for col in categorical_cols:
+    df_cleaned[col] = df_cleaned[col].astype('category')
+print(f"-> Converted all Object-type columns ({len(categorical_cols)}) to Category type ")
+print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+# - Compare Mean, Median
+print("# - Compare Mean, Median")
+mean_age = df_cleaned['Age'].mean()
+median_age = df_cleaned['Age'].median()
+print(f"Age - Mean: {mean_age:.2f}, Median: {median_age:.2f}")
+
+if 'TotalKg' in df_cleaned.columns:
+    mean_total = df_cleaned['TotalKg'].mean()
+    median_total = df_cleaned['TotalKg'].median()
+    print(f"TotalKg - Mean: {mean_total:.2f}, Median: {median_total:.2f}")
+print("-" * 50)
+print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+#LAB_04 Feature Engineering
+#Label Encoding 
+le = LabelEncoder()
+if 'Division' in df_cleaned.columns:
+    df_cleaned['Division_Encoded'] = le.fit_transform(df_cleaned['Division'])
+    print("-> Label Encoding Completed for 'Division'")
+    print("   Mapping:", dict(zip(le.classes_, le.transform(le.classes_))))
+print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+# 2. One-Hot Encoding 
+print("--- เริ่มจัดการปัญหา High Cardinality ในคอลัมน์ Division ---")
+
+top_divisions = df_cleaned['Division'].value_counts().index[:30]
+
+df_cleaned['Division'] = df_cleaned['Division'].apply(lambda x: x if x in top_divisions else 'Other')
+
+print(f"ลดรูป Division เหลือทั้งหมด: {df_cleaned['Division'].nunique()} กลุ่ม (รวม 'Other')")
+
+df_final = pd.get_dummies(df_cleaned, columns=['Division'], prefix='Div', dtype=int)
+
+print("--- ทำ One-Hot Encoding สำเร็จแล้ว! ---")
+print(f"ขนาดของ DataFrame ใหม่: {df_final.shape}")
